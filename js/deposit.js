bfeef3ebd165a62910d3fa44b4323676c6d67236
SQLAdmin <abuyeahia24@gmail.com>
2023-02-12 17:33:42 +0600

    all done added withdraw and deposit operation

--- js/deposit.js
+++ js/deposit.js
@@ -1,17 +1,39 @@
-// step 1: add events listener to the deposit button
-document.getElementById('btn-deposit').addEventListener('click', function(){
+// step-1: add events listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function () {
     // console.log('button click')
-    // step 2: get the deposit amount from the deposit field
+    // step-2: get the deposit amount from the deposit field
     // for input field use .value to the inside the input field
     const depositFiled = document.getElementById('deposit-field');
-    const depositAmount = depositFiled.value;
+    const newDepositAmountString = depositFiled.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    // console.log(typeof newDepositAmount);
     // console.log(depositAmount);
 
-    // step 3: get the current deposit total amount
+    // step-3: get the current deposit total amount
     // for non-input(element other than input, textarea) use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousAmount = parseFloat(previousDepositTotalString);
+    // console.log(typeof previousAmount);
     // console.log(depositTotal);
-    depositTotalElement.innerText = depositAmount;
+       
+    //  step-4: add numbers to set the total deposit
+    const currentDepositTotal = previousAmount + newDepositAmount;
+     
+    //  set the deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // step-5 get balance current total
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-6: calculate current total balance 
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    //  set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+    // step-7: clear deposit field
+    depositFiled.value = '';
+   
 
 })
---
+++ js/withdraw.js
@@ -0,0 +1,38 @@
+// added event handler with id
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // console.log("ballll");
+    //step-2: get the withdraw amount from the withdraw input field
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    // also make use to convert the input into a number by string parsefloat
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    // console.log(newWithdrawAmount);
+
+    // step-3: get previous withdraw total
+    const WithdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = WithdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    // console.log(previousWithdrawTotal);
+
+    // step-4: calculate total withdraw amount + step-4: set total withdraw amount
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    WithdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step-5: get the previous total balance
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    // console.log(previousBalanceTotal);
+    
+    // step-6: calculate new balance total  // 5-6 set the new balance total
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    // step-7: remove input value
+    withdrawField.value = "";
+
+
+
+})
+
