a551f537553971f7528794299c3bc584f3bf6c1d
SQLAdmin <abuyeahia24@gmail.com>
2023-02-12 15:27:24 +0600

    js added with authentication

--- js/login.js
+++ js/login.js
@@ -1,3 +1,29 @@
+// step 1: add click event handler with the submit button
+
 document.getElementById('btn-submit').addEventListener('click', function(){
-    console.log('submit button slicked');
+   // step-2: get the email address inside with email input field
+   // always remember to use .value to get text from an input field
+   const emailField = document.getElementById('user-email');
+   const email = emailField.value;
+   // console.log(email);
+
+   // step: 3
+   //3.a set id on the html element of input password
+   //3.b get the element
+   // 3.c get the value from password element
+   const passwordField = document.getElementById('user-password');
+   const password = passwordField.value;
+   // console.log(password);
+   // DANGER: DO NOT VERIFY EMAIL PASSWORD ON THE CLIEN SIDE
+   // step:4 verify email and password
+
+   if(email=='galib@gmail.com' && password == 'Galib123'){
+      // console.log('valid user');
+      window.location.href = 'bank.html'
+   }
+   else{
+      alert('detect problem');
+   }
+
 })
+
